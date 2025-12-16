aa35d498cadf954f4411e5523e6c381e1ce4af7f
Gabriel0955 <aguirregabriel0204@gmail.com>
2025-12-16 01:33:26 -0500

    Desarrollo

--- public/tasks/quick_tasks.php
+++ public/tasks/quick_tasks.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../../src/auth.php';
 require_once __DIR__ . '/../../config.php';
 require_once __DIR__ . '/../../services/QuickTaskService.php';
+require_once __DIR__ . '/../../src/db.php';
 $pdo = get_pdo();
 $quickTaskService = new QuickTaskService($pdo);
 $userId = $_SESSION['user_id'];
